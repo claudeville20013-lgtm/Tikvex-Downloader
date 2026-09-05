3bf857c7b9a50ddb2b6facec1818adcd91db54f5
Ssempebwa Claude <claudeville20013@gmail.com>
2026-09-06 01:23:01 +0300

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('a[href^="#"]').forEach(a=>{
-  a.addEventListener('click',e=>{
-    const target=document.querySelector(a.getAttribute('href'));
-    if(target){e.preventDefault();target.scrollIntoView({behavior:'smooth',block:'start'});}
-  });
-});
